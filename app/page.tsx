ce26be5e5a622724550add12053e82c107927082
kangalio <me@kangalio.net>
2025-01-22 23:40:47 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Drawer } from "@mui/material";
+
+export default async function Home() {
+    return (
+        <Drawer variant="permanent" anchor="left">
+            hello
+        </Drawer>
+    );
+}
